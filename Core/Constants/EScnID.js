2379da93b63761b10e91d2f3c47857a4d96953d2
S4LeagueOpenSource <s4lopensource@gmail.com>
2016-04-06 21:12:24 +0200

    - Removed SCN shits

--- Core/Constants/EScnID.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	Root: 1652086138,
-	Bone: 1832983249,
-	Bonesystem: 1584673599,
-	Mesh: 135305464,
-	Spawn: 632156369
-}
--- Core/Constants/EScnVersion.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	Version1: 1036831949,
-	Version2: 1045220557
-}
